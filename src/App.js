d1b368d45ab39b8ec1502cff8822b195ceac6b63
ThakurSaad <thakursaad613@gmail.com>
2023-02-18 22:35:43 +0600

    Basic context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { createContext, useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
 
+const PRODUCT_CONTEXT = createContext();
+
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/products")
+      .then((res) => res.json())
+      .then((data) => setData(data.data));
+  }, []);
+
+  const value = {
+    data,
+  };
+
   return (
     <div>
-      <RouterProvider router={routes} />
+      <PRODUCT_CONTEXT.Provider value={value}>
+        <RouterProvider router={routes} />
+      </PRODUCT_CONTEXT.Provider>
     </div>
   );
 }
