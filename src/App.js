b82f33198f0b8d9ad18b4b96a17fc55283b1b8bc
ThakurSaad <thakursaad613@gmail.com>
2023-02-18 23:05:42 +0600

    Context best practice

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import { createContext, useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
+import ProductProvider from "./context/ProductProvider";
 import routes from "./routes/routes";
 
-const PRODUCT_CONTEXT = createContext();
-
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/products")
-      .then((res) => res.json())
-      .then((data) => setData(data.data));
-  }, []);
-
-  const value = {
-    data,
-  };
-
   return (
     <div>
-      <PRODUCT_CONTEXT.Provider value={value}>
+      <ProductProvider>
         <RouterProvider router={routes} />
-      </PRODUCT_CONTEXT.Provider>
+      </ProductProvider>
     </div>
   );
 }
